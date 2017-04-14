2ce7498fbf017202d1ca0f3159c9a02141d248af
Bronson Brown <bronson1998@live.com>
2017-04-13 23:40:52 -0400

    Day One stopping point

--- classicsnake.cpp
+++ classicsnake.cpp
@@ -1,24 +1,42 @@
 // classicsnake.cpp : Defines the entry point for the console application.
-//
+//Console Snake by [B] [R] [O] [N] [S] [O] [N]   [B] [R] [O] [W] [N]. 2017... 
 
 #include "stdafx.h"
 #include <iostream>
 using namespace std;
 bool gameOver;
+//Creates coordinate plane.
 const int height = 20;
 const int width = 20;
+//Numerical variables
+int x, y, fruitX, fruitY, score;
+// index direction for snake
+enum direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
+direction dir;
 
 // Four basic game structures.
 //Establishes a basis for starting the game.
 void setup()
 {
 	gameOver = false;
+	dir = stop;
+	x = width / 2;
+	y = height / 2;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score = 0;
+	system('cls');
 
 }
 //Draws changes to field.
 void draw()
 {
-
+	for (int i = 0, i < width, i++)
+		cout "#";
+	cout << endl;
+	for (int i = 0, i < width, i++)
+		cout "#";
+	count << endl;
 }
 //takes player input.
 void input()
